eb2aeb4c283ceecf3cfbe3302e150e8be237efe6
damien <damienhiggins15@gmail.com>
2026-03-01 09:20:40 +0000

    now able to search for animes with same genres as one that the user enteres, i plan to improve this by make sure it maches 2 or more of the genres

--- recommender.py
+++ recommender.py
@@ -10,6 +10,7 @@ df = pd.read_csv("anime-dataset-2023.csv")
 def cli():
     pass
 
+#region  genre
 @cli.command()
 @click.argument("genre", nargs=-1)
 def genre(genre):
@@ -34,8 +35,9 @@ def genre(genre):
         )
 
     console.print(table)
+#endregion
 
-
+# region title
 @cli.command()
 @click.argument("title", nargs=-1)
 def title(title):
@@ -46,25 +48,93 @@ def title(title):
 
     console = Console()
     table = Table(title=f"Results for '{title}'")
+    table.add_column("Row", style="red")
     table.add_column("Title", style="cyan")
     table.add_column("Genres", style="green")
     table.add_column("Score", style="yellow")
     table.add_column("Episodes", style="magenta")
     table.add_column("Type", style="blue")
 
+    counter = 0
+
     for _, row in df_filtered.iterrows():
+        counter += 1
         table.add_row(
+            str(counter),
             str(row["English name"]) if row["English name"] != "UNKNOWN" else str(row["Name"]),
             str(row["Genres"]),
             str(row["Score"]),
             str(row["Episodes"]),
             str(row["Type"])
+        ) 
+        table.add_row(
+            str("-")
         )
     console.print(table)
+#endregion
+
+#region recommend
+@cli.command()
+@click.argument("recommend", nargs=-1)
+def recommend(recommend):
+    recommend = " ".join(recommend)
+    df_filtered = df[df["English name"].str.contains(str(recommend), na=False, case=False)].copy()
+
+    genres = []
+
+    for _, row in df_filtered.iterrows():
+
+        _genres = row["Genres"].split(", ")
+        for genre in _genres:
+            if genre not in genres and genre != "UNKNOWN":
+                genres.append(genre)
+
+    print(genres)
+
+    df_filtered = df[df["Genres"].isin(genres)]
+    df_filtered = df_filtered.copy()
+    df_filtered["Score"] = pd.to_numeric(df_filtered["Score"], errors="coerce")
+    df_filtered = df_filtered.sort_values("Score", ascending=False)
+    df_filtered = df_filtered.head(10)
+
+    console = Console()
+    table = Table(title=f"Results for '{title}'")
+    table.add_column("Row", style="red")
+    table.add_column("Title", style="cyan")
+    table.add_column("Genres", style="green")
+    table.add_column("Score", style="yellow")
+    table.add_column("Episodes", style="magenta")
+    table.add_column("Type", style="blue")
+
+    counter = 0
+
+    for _, row in df_filtered.iterrows():
+
+        counter += 1
+
+        table.add_row(
+            str(counter),
+            str(row["English name"]) if row["English name"] != "UNKNOWN" else str(row["Name"]),
+            str(row["Genres"]),
+            str(row["Score"]),
+            str(row["Episodes"]),
+            str(row["Type"])
+        ) 
+        table.add_row(
+            str("-")
+        )
+    console.print(table)
+    
+#endregion
+
+
 
 if __name__ == "__main__":
     while True:
         user_input = input("Enter command: ")
         if user_input == "quit":
             break
+        elif user_input == "clear":
+            print("\x1b[2J\x1b[H",end="")
+            continue
         cli(shlex.split(user_input), standalone_mode=False)
